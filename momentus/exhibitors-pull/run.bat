4880d2cd40071e5203f2274bb375ab4ff2310399
Kyle Poteete <kylep@kallman.local>
2026-06-09 16:53:17 -0400

    Add startup message to exhibitors pull and autoclose bat

--- momentus/exhibitors-pull/run.bat
+++ momentus/exhibitors-pull/run.bat
@@ -2,4 +2,3 @@
 cd /d "%~dp0"
 echo Starting Exhibitors Pull...
 dotnet run
-pause
--- momentus/exhibitors-pull/run.ps1
+++ momentus/exhibitors-pull/run.ps1
@@ -1,5 +1,6 @@
 $ErrorActionPreference = "Stop"
 
+Write-Host "GETTING STARTED"
 Write-Host "Starting Exhibitors Pull..."
 Write-Host "Folder: $PSScriptRoot"
 
